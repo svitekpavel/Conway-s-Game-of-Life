2814964e78768645cb6ee35a074a9ba0b4d50ae7
Pavel Svitek <svitek.pavel@gmail.com>
2017-01-14 13:30:26 +0100

    added 'game-of-life' route

--- src/routes.js
+++ src/routes.js
@@ -4,13 +4,15 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import HomePage from './components/HomePage';
 import FuelSavingsPage from './containers/FuelSavingsPage'; // eslint-disable-line import/no-named-as-default
-import AboutPage from './components/AboutPage.js';
-import NotFoundPage from './components/NotFoundPage.js';
+import AboutPage from './components/AboutPage';
+import GameOfLifePage from './components/GameOfLifePage';
+import NotFoundPage from './components/NotFoundPage';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage}/>
     <Route path="fuel-savings" component={FuelSavingsPage}/>
+    <Route path="game-of-life" component={GameOfLifePage}/>
     <Route path="about" component={AboutPage}/>
     <Route path="*" component={NotFoundPage}/>
   </Route>
