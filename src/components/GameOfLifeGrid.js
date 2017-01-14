8c7e42b14412f2a502ba6216919983eb6448c1a0
Pavel Svitek <svitek.pavel@gmail.com>
2017-01-14 16:10:09 +0100

    added components/GameOfLifeGrid.js

---
+++ src/components/GameOfLifeGrid.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import uuid from 'uuid';
+
+import GridCell from './GridCell';
+
+class GameOfLifeGrid extends React.Component {
+  renderRow(row) {
+    const rowNodes = row.map((c) => <GridCell cell={c} key={uuid.v4()}/>);
+    return <div className="row">{rowNodes}</div>;
+  }
+  renderGrid(grid) {
+    return grid.map((r) => this.renderRow(r));
+  }
+  render() {
+    const grid = this.renderGrid(this.props.epoch);
+    return <div className="grid">{grid}</div>;
+  }
+}
+
+GameOfLifeGrid.propTypes = {
+  epoch: React.PropTypes.array,
+};
+
+export default GameOfLifeGrid;
