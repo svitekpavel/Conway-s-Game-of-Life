147da1b284d6548ebacdf28ecf0fbe603dbf68b9
Pavel Svitek <svitek.pavel@gmail.com>
2017-01-14 18:53:50 +0100

    added bigger grid with some patterns to GameOfLifePage

--- src/containers/GameOfLifePage.js
+++ src/containers/GameOfLifePage.js
@@ -7,6 +7,8 @@ import {
   setEvolutionRunning,
 } from '../actions/gameOfLifeActions';
 import getNextEpoch from '../utils/getNextEpoch';
+import createGrid from '../utils/createGrid';
+import gridInsertPattern from '../utils/gridInsertPattern';
 
 class GameOfLifePage extends React.Component {
   constructor(props) {
@@ -16,6 +18,16 @@ class GameOfLifePage extends React.Component {
   }
   componentDidMount() {
     // you can start evolution here
+    const { dispatch } = this.props;
+
+    // create grid and insert some patterns
+    let epoch = createGrid(30, 30);
+    epoch = gridInsertPattern('blinker2', epoch, 13, 15);
+    epoch = gridInsertPattern('glider', epoch, 10, 0);
+    epoch = gridInsertPattern('lwss', epoch, 0, 0);
+    epoch = gridInsertPattern('beacon', epoch, 20, 20);
+
+    dispatch(setEpoch(epoch));
   }
   componentWillUnmount() {
     clearInterval(this.evolutionInterval);
@@ -52,8 +64,8 @@ class GameOfLifePage extends React.Component {
 
 GameOfLifePage.propTypes = {
   epoch: React.PropTypes.array.isRequired,
-  evolutionRunning: React.PropTypes.bool,
-  dispatch: React.PropTypes.function,
+  evolutionRunning: React.PropTypes.bool.isRequired,
+  dispatch: React.PropTypes.func.isRequired,
 };
 
 function mapStateToProps(state) {
