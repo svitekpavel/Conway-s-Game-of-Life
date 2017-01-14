321a997fcb1e93ff6d1e0c60ddc42eb86ed1211d
Pavel Svitek <svitek.pavel@gmail.com>
2017-01-14 18:05:42 +0100

    removed logic from GameOfLife component

--- src/components/GameOfLife.js
+++ src/components/GameOfLife.js
@@ -1,38 +1,8 @@
 import React from 'react';
 
 import GameOfLifeGrid from './GameOfLifeGrid';
-import getNextEpoch from '../utils/getNextEpoch';
-// import createGrid from '../utils/createGrid';
 
-const initialGrid = [
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-];
-
-class GameOfLifePage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      grid: initialGrid,
-      // grid: createGrid(50, 50),
-    };
-  }
-  componentDidMount() {
-    // start the evolution here
-    this.evolutionInterval = setInterval(() => {
-      this.setState({
-        grid: getNextEpoch(this.state.grid),
-      });
-    }, 500);
-  }
-  componentWillUnmount() {
-    clearInterval(this.evolutionInterval);
-  }
-
+class GameOfLife extends React.Component {
   render() {
     return (
       <div>
@@ -40,15 +10,16 @@ class GameOfLifePage extends React.Component {
           <button onClick={this.props.onStartEvolution}>Start evolution</button>
           <button onClick={this.props.onStopEvolution}>Stop evolution</button>
         </div>
-        <GameOfLifeGrid epoch={this.state.grid}/>
+        <GameOfLifeGrid epoch={this.props.epoch}/>
       </div>
     );
   }
 }
 
-GameOfLifePage.propTypes = {
+GameOfLife.propTypes = {
+  epoch: React.PropTypes.array.isRequired,
   onStartEvolution: React.PropTypes.function,
   onStopEvolution: React.PropTypes.function,
 };
 
-export default GameOfLifePage;
+export default GameOfLife;
