67fb99681016441f6fe4642f27192682ce5b1cd4
Pavel Svitek <svitek.pavel@gmail.com>
2017-01-16 20:48:04 +0100

    added short bio to AboutPage

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,18 +1,48 @@
 import React from 'react';
-import {Link} from 'react-router';
+
 import '../styles/about-page.css';
 
 // Since this component is simple and static, there's no parent container for it.
 const AboutPage = () => {
   return (
     <div>
-      <h2 className="alt-header">About</h2>
+      <div className="sidebar">
+        <img src="https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/2/005/092/324/264bf24.jpg" alt="Pavel Svitek photo" className="photo"/>
+      </div>
+      <h1>Pavel Svitek</h1>
+
       <p>
-        This example app is part of the <a href="https://github.com/coryhouse/react-slingshot">React-Slingshot
-        starter kit</a>.
+        I am a young entrepreneur-minded fullstack developer, who came a long way
+        from python developer to managing teams and building projects
+        for international markets.
       </p>
       <p>
-        <Link to="/badlink">Click this bad link</Link> to see the 404 page.
+        Across the way, I've gained experience from python programming,
+        marketing, managing people, crafting the business model.. and therefore
+        I have insights from different points of view on building web
+        products/services.
+      </p>
+      <p>
+        Over 8+ years experience in building online web applications, designing
+        products, setting up the business processes, motivating people, leading
+        teams and more.
+      </p>
+      <p>
+        My life is based on three major principles:
+        <ul>
+          <li>Think outside the box - Think in a different way and you will find innovative, unconventional solutions.</li>
+          <li>Deliver extraordinary results - If you stop thinking conventionally, you'll deliver extraordinary results.</li>
+          <li>Travel, learn, discover - Collect experiences, not things. Have stories to tell, not stuff to show.</li>
+        </ul>
+      </p>
+      <p>
+        Get in touch on
+        {' '}
+        <a href="mailto:pavel@pavelsvitek.com">pavel@pavelsvitek.com</a>
+        {' '}
+        or Whatsapp me on
+        {' '}
+        <a href="tel:+421904642477"> +421 904 64 24 77</a>.
       </p>
     </div>
   );
