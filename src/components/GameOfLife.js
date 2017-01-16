65b14db13ae2498424b7f051fb757d412f5cd3f9
Pavel Svitek <svitek.pavel@gmail.com>
2017-01-16 19:45:39 +0100

    added note to GameOfLife component

--- src/components/GameOfLife.js
+++ src/components/GameOfLife.js
@@ -9,6 +9,11 @@ class GameOfLife extends React.Component {
       <div>
         <div className="">
           <h1>Game of a <span className="colorful">Colorful</span> Life :-)</h1>
+          <span>
+            Be patient: If you wait long enough, you will see some nice shapes,
+            flowers, evolving gliders and part of a heart before the very end
+            of the evolution.
+          </span>
         </div>
         <div className="toolbar">
           {!evolutionRunning && <button onClick={this.props.onStartEvolution} className="start">Start evolution</button>}
