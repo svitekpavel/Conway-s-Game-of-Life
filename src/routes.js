2df9ed6f9755ee2adfd92862104390c47f0e4df4
Pavel Svitek <svitek.pavel@gmail.com>
2017-01-16 19:31:07 +0100

    removed HomePage import from routes

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/App';
-import HomePage from './components/HomePage';
 import AboutPage from './components/AboutPage';
 import GameOfLifePage from './containers/GameOfLifePage';
 import NotFoundPage from './components/NotFoundPage';
