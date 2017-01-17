686c6752ab0658c37485b2773e4176fb881817f6
Pavel Svitek <svitek.pavel@gmail.com>
2017-01-17 20:06:56 +0100

    added tests for GameOfLife

---
+++ src/components/__tests__/GameOfLife.spec.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import chai, { expect } from 'chai';
+import chaiEnzyme from 'chai-enzyme'
+
+import GameOfLife from '../GameOfLife';
+import GameOfLifeGrid from '../GameOfLifeGrid';
+
+chai.use(chaiEnzyme())
+
+function getShallowedComponent(epoch, evolutionRunning=false) {
+  return shallow(
+    <GameOfLife
+      evolutionRunning={evolutionRunning}
+      epoch={epoch}
+      />
+  )
+}
+
+describe('<GameOfLife />', () => {
+  const epoch = [
+    [0, 1, 0],
+    [0, 1, 0],
+    [0, 1, 0],
+  ];
+
+  it('should have exactly 1 div with className=\'header\'', () => {
+    const wrapper = getShallowedComponent(epoch, false);
+    const node = wrapper.find('.header');
+
+    expect(node).to.have.length(1);
+  });
+
+  it('should have exactly 1 div with className=\'toolbar\'', () => {
+    const wrapper = getShallowedComponent(epoch, false);
+    const node = wrapper.find('.toolbar');
+
+    expect(node).to.have.length(1);
+  });
+
+  it('should have exactly 1 button with className=\'start\' and text \'Start evolution\'', () => {
+    const wrapper = getShallowedComponent(epoch, false);
+    const node = wrapper.find('button');
+
+    expect(node).to.have.className('start');
+    expect(node).to.have.text('Start evolution');
+  });
+
+  it('should have exactly 1 button with className=\'stop\' and text \'Start evolution\'', () => {
+    const wrapper = getShallowedComponent(epoch, true);
+    const node = wrapper.find('button');
+
+    expect(node).to.have.className('stop');
+    expect(node).to.have.text('Stop evolution');
+  });
+
+  it('should have exactly 1 GameOfLifeGrid component', () => {
+    const wrapper = shallow(<GameOfLife epoch={epoch}/>);
+    const nodes = wrapper.find(GameOfLifeGrid);
+
+    expect(nodes).to.have.length(1);
+  });
+});
