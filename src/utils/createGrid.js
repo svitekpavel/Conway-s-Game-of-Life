fd13645fe403a0002328b34f9f95c3baf386cafe
Pavel Svitek <svitek.pavel@gmail.com>
2017-01-17 20:21:55 +0100

    added description to createGrid function

--- src/utils/createGrid.js
+++ src/utils/createGrid.js
@@ -1,3 +1,6 @@
+/**
+ * Creates a 2-dimensional array of size [x, y] filled with zeros
+ */
 export default function createGrid (x, y) {
   return Array.apply(null, Array(y)).map(() => Array.apply(null, Array(x)).map(Number.prototype.valueOf,0));
 }
