d1965abeded60fd4a14cbf91f2984dd8d3f3ca79
Pavel Svitek <svitek.pavel@gmail.com>
2017-01-17 19:48:09 +0100

    added GridCell tests

---
+++ src/components/__tests__/GridCell.spec.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import chai, { expect } from 'chai';
+import chaiEnzyme from 'chai-enzyme'
+
+import GridCell from '../GridCell';
+
+chai.use(chaiEnzyme())
+
+describe('<GridCell />', () => {
+  it('should have exactly one span with className=\'cell\'', () => {
+    const wrapper = shallow(<GridCell cell={0}/>);
+    const node = wrapper.find('span');
+
+    expect(node).to.have.length(1);
+    expect(node).to.have.className('cell');
+  });
+
+  it('with cell={1} should have className \'live\'', () => {
+    const wrapper = shallow(<GridCell cell={1}/>);
+    const node = wrapper.find('span');
+
+    expect(node).to.have.className('live');
+  });
+
+  it('with cell={0} should not have className \'live\'', () => {
+    const wrapper = shallow(<GridCell cell={0}/>);
+    const node = wrapper.find('span');
+
+    expect(node).to.not.have.className('live');
+  });
+});
