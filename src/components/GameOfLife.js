45025fec449b51f74ec1faa84c09a8063963158c
Pavel Svitek <svitek.pavel@gmail.com>
2017-01-17 19:58:43 +0100

    added class .header to GameOfLife component

--- src/components/GameOfLife.js
+++ src/components/GameOfLife.js
@@ -7,7 +7,7 @@ class GameOfLife extends React.Component {
     const { epoch, evolutionRunning } = this.props;
     return (
       <div>
-        <div className="">
+        <div className="header">
           <h1>Game of a <span className="colorful">Colorful</span> Life :-)</h1>
           <span>
             Be patient: If you wait long enough, you will see some nice shapes,
