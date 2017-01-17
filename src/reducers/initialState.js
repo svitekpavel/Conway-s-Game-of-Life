99d9b602996e6c970d90ded88e428fc9905ece35
Pavel Svitek <svitek.pavel@gmail.com>
2017-01-17 20:07:39 +0100

    removed legacy code from initialState.js

--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -7,22 +7,6 @@ const initialEpoch = [
 ];
 
 export default {
-  fuelSavings: {
-    newMpg: '',
-    tradeMpg: '',
-    newPpg: '',
-    tradePpg: '',
-    milesDriven: '',
-    milesDrivenTimeframe: 'week',
-    displayResults: false,
-    dateModified: null,
-    necessaryDataIsProvidedToCalculateSavings: false,
-    savings: {
-      monthly: 0,
-      annual: 0,
-      threeYear: 0
-    }
-  },
   gameOfLife: {
     epoch: initialEpoch,
     evolutionRunning: false,
