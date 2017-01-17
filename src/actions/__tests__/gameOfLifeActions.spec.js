4e13bd6901eb2bbb05ada05f638e6c1797841e8b
Pavel Svitek <svitek.pavel@gmail.com>
2017-01-17 20:19:46 +0100

    adjusted tests for gameOfLifeActions

--- src/actions/__tests__/gameOfLifeActions.spec.js
+++ src/actions/__tests__/gameOfLifeActions.spec.js
@@ -13,25 +13,30 @@ import {
 describe('gameOfLifeActions', () => {
   describe('setEpoch', () => {
     it('should return action with type=SET_EPOCH', () => {
-      expect(setEpoch().type).to.equal(SET_EPOCH);
+      const action = setEpoch();
+      expect(action.type).to.equal(SET_EPOCH);
     });
 
     it('should return action with epoch=[0, 1, 0, 0]', () => {
-      expect(setEpoch([0, 1, 0, 0]).epoch).to.deep.equal([0, 1, 0, 0]);
+      const action = setEpoch([0, 1, 0, 0]);
+      expect(action.epoch).to.deep.equal([0, 1, 0, 0]);
     });
   });
 
   describe('setEvolutionRunning', () => {
     it('should return action with type=SET_EVOLUTION_RUNNING', () => {
-      expect(setEvolutionRunning().type).to.equal(SET_EVOLUTION_RUNNING);
+      const action = setEvolutionRunning();
+      expect(action.type).to.equal(SET_EVOLUTION_RUNNING);
     });
 
     it('should return action with evolutionRunning=true', () => {
-      expect(setEvolutionRunning(true).evolutionRunning).to.equal(true);
+      const action = setEvolutionRunning(true);
+      expect(action.evolutionRunning).to.equal(true);
     });
 
     it('should return action with evolutionRunning=false', () => {
-      expect(setEvolutionRunning(false).evolutionRunning).to.equal(false);
+      const action = setEvolutionRunning(false);
+      expect(action.evolutionRunning).to.equal(false);
     });
   });
 
