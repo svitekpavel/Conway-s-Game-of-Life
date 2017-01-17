4f4c8c7194679d4cdc2bcaf3a9585909a04103fc
Pavel Svitek <svitek.pavel@gmail.com>
2017-01-17 19:28:57 +0100

    added gameOfLifeActions.spec.js tests

---
+++ src/actions/__tests__/gameOfLifeActions.spec.js
@@ -0,0 +1,40 @@
+import { expect } from 'chai';
+
+import {
+  SET_EPOCH,
+  SET_EVOLUTION_RUNNING,
+} from '../../constants/actionTypes';
+import {
+  setEpoch,
+  setEvolutionRunning,
+} from './../gameOfLifeActions';
+
+
+// For names used in tests see
+// https://en.wikipedia.org/wiki/Conway's_Game_of_Life#ExamplesOfPatterns
+describe('gameOfLifeActions', () => {
+  describe('setEpoch', () => {
+    it('should return action with type=SET_EPOCH', () => {
+      expect(setEpoch().type).to.equal(SET_EPOCH);
+    });
+
+    it('should return action with epoch=[0, 1, 0, 0]', () => {
+      expect(setEpoch([0, 1, 0, 0]).epoch).to.deep.equal([0, 1, 0, 0]);
+    });
+  });
+
+  describe('setEvolutionRunning', () => {
+    it('should return action with type=SET_EVOLUTION_RUNNING', () => {
+      expect(setEvolutionRunning().type).to.equal(SET_EVOLUTION_RUNNING);
+    });
+
+    it('should return action with evolutionRunning=true', () => {
+      expect(setEvolutionRunning(true).evolutionRunning).to.equal(true);
+    });
+
+    it('should return action with evolutionRunning=false', () => {
+      expect(setEvolutionRunning(false).evolutionRunning).to.equal(false);
+    });
+  });
+
+});
